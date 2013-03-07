d83c5a1ad201b99f968df1e51453d33e2a07d765
Manuel Simoni <msimoni@gmail.com>
2013-03-07 17:43:55 +0100

    Primitive abstract interpretation.

--- taf.js
+++ taf.js
@@ -1,53 +1,84 @@
 function taf_init() {
     /***** Types *****/
-    function is_instance(object, type) { return object.constructor === type; }
-    function assert_type(object, type) { assert(is_instance(object, type)); }
+    function type_of(object) { return object.constructor; }
+    function is_instance(object, type) { return type_of(object) === type; }
+    function assert_type(object, type) { assert(is_instance(object, type)); return object; }
     function t_cons(car, cdr) { this.car = car; this.cdr = cdr; }
     function t_symbol(jsstr) { this.jsstr = jsstr; }
     function t_nil() {}
-    function symbol(jsstr) { return new t_symbol(jsstr); }
-    function cons(car, cdr) { return new t_cons(car, cdr); }
+    function t_unit() {}
     function car(cons) { assert_type(cons, t_cons); return cons.car; }
     function cdr(cons) { assert_type(cons, t_cons); return cons.cdr; }
+    function make_cons(car, cdr) { return new t_cons(car, cdr); }
+    function make_symbol(jsstr) { return new t_symbol(jsstr); }
+    function symbol_name(symbol) { assert_type(symbol, t_symbol); return symbol.jsstr; }
     var nil = new t_nil();
+    var unit = new t_unit();
     function equals(a, b) { return a.equals_impl(a, b); }
     t_cons.prototype.equals_impl = function(cons, other) {
         if (is_instance(other, t_cons)) return equals(cons.car, other.car) && equals(cons.cdr, other.cdr);
         else return false; };
     t_symbol.prototype.equals_impl = function(symbol, other) {
-        if (is_instance(other, t_symbol)) return symbol.jsstr === other.jsstr;
+        if (is_instance(other, t_symbol)) return symbol_name(symbol) === symbol_name(other);
         else return false; };
     t_nil.prototype.equals_impl = function(nil, other) { return nil === other; };
+    t_unit.prototype.equals_impl = function(unit, other) { return unit === other; };
     /***** Built-in Symbols *****/
-    var sym_begin = symbol("begin");
+    var sym_begin = make_symbol("begin");
     /***** Parser *****/
     function parse(string) {
         var res = p_program(ps(string));
-	if (res.remaining.index === string.length) return cons(sym_begin, array_to_list(res.ast));
+	if (res.remaining.index === string.length) return make_cons(sym_begin, array_to_list(res.ast));
         else fail("parse error at " + res.remaining.index); }
     var p_expression = function(input) { return p_expression(input); }; // forward decl.
     var p_symbol_char = choice(range("a", "z"), "-");
-    var p_symbol = action(join_action(repeat1(p_symbol_char), ""), symbol);
+    var p_symbol = action(join_action(repeat1(p_symbol_char), ""), make_symbol);
     var p_compound = action(wsequence("(", repeat0(p_expression), ")"),
                             function(ast) { return array_to_list(ast[1]); });
     var p_expression = whitespace(choice(p_symbol, p_compound));
     var p_program = whitespace(repeat0(p_expression));
+    /***** (Abstract) Interpretation *****/
+    function interpret(interpreter, expression) {
+        if (is_instance(expression, t_cons)) return interpret_cons(interpreter, expression);
+        else return interpreter.interpret_self_evaluating_object(interpreter, expression); }
+    function interpret_cons(interpreter, expression) {
+        var operator = car(expression);
+        if (is_instance(operator, t_symbol)) {
+            var special = lookup_special(interpreter, operator);
+            if (special !== undefined) return special(interpreter, cdr(expression));
+            else fail("Illegal operator: " + expression); }
+        else fail("Illegal form: " + expression); }
+    // Special forms
+    function special_name(symbol) { return "interpret_special_" + symbol_name(symbol); }
+    function lookup_special(interpreter, symbol) { return interpreter[special_name(symbol)]; }
+    function put_special(interpreter_type, symbol, fun) { interpreter_type.prototype[special_name(symbol)] = fun; }
+    /***** Evaluator *****/
+    function evaluate(expression) { return interpret(new t_evaluator(), expression); }
+    function t_evaluator() {}
+    t_evaluator.prototype.interpret_self_evaluating_object = function(evaluator, object) {
+        return object;
+    };
+    put_special(t_evaluator, sym_begin, function self(evaluator, body) {
+        return unit;
+    });
     /***** Utilities *****/
     function assert(b) { if (!b) fail("Assertion failed!"); }
     function fail(msg) { throw msg; }
     function array_to_list(array, end) {
-	var c = end ? end : nil; for (var i = array.length; i > 0; i--) c = cons(array[i - 1], c); return c; }
-    function list() { return array_to_list(Array.prototype.slice.call(arguments, 0)); }
+	var c = end ? end : nil; for (var i = array.length; i > 0; i--) c = make_cons(array[i - 1], c); return c; }
+    function make_list() { return array_to_list(Array.prototype.slice.call(arguments, 0)); }
     /***** API *****/
     return {
         "assert": assert,
         "car": car,
         "cdr": cdr,
-        "cons": cons,
         "equals": equals,
-        "list": list,
-        "symbol": symbol,
+        "evaluate": evaluate,
+        "make_cons": make_cons,
+        "make_list": make_list,
+        "make_symbol": make_symbol,
         "nil": nil,
         "parse": parse,
+        "unit": unit,
     };
 }
--- test.js
+++ test.js
@@ -1,8 +1,17 @@
 var taf = taf_init();
 
+taf.assert(taf.equals(taf.nil, taf.nil));
+taf.assert(!taf.equals(taf.nil, taf.unit));
+taf.assert(!taf.equals(taf.unit, taf.nil));
+taf.assert(taf.equals(taf.unit, taf.unit));
+taf.assert(taf.equals(taf.make_cons(taf.unit, taf.nil), taf.make_cons(taf.unit, taf.nil)));
+taf.assert(!taf.equals(taf.make_cons(taf.nil, taf.unit), taf.make_cons(taf.unit, taf.nil)));
+taf.assert(taf.equals(taf.make_list(taf.unit, taf.unit), taf.make_list(taf.unit, taf.unit)));
+taf.assert(!taf.equals(taf.make_list(taf.unit, taf.unit, taf.nil), taf.make_list(taf.unit, taf.unit)));
+
 function test_empty_program(string) {
     var form = taf.parse(string);
-    taf.assert(taf.equals(form, taf.list(taf.symbol("begin"))));
+    taf.assert(taf.equals(form, taf.make_list(taf.make_symbol("begin"))));
 }
 test_empty_program("");
 test_empty_program("   ");
@@ -14,9 +23,9 @@ test_empty_program("  \t \r\n");
 
 function test_foobar_program(string) {
     var form = taf.parse(string);
-    taf.assert(taf.equals(form, taf.list(taf.symbol("begin"),
-                                         taf.list(taf.symbol("foo"),
-                                                  taf.list(taf.symbol("bar"))))));
+    taf.assert(taf.equals(form, taf.make_list(taf.make_symbol("begin"),
+                                              taf.make_list(taf.make_symbol("foo"),
+                                                            taf.make_list(taf.make_symbol("bar"))))));
 }
 test_foobar_program("( foo   ( bar))");
 test_foobar_program("(foo \n (bar\t) )");
@@ -25,3 +34,5 @@ test_foobar_program("\t (foo\n(bar\t)\t)");
 test_foobar_program(" (   foo (bar))");
 test_foobar_program("  \t \n(\n\rfoo (bar ) )");
 test_foobar_program("  (foo (bar))");
+
+taf.assert(taf.equals(taf.evaluate(taf.parse("(begin)")), taf.unit));
